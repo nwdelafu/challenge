34c334e77d87874fab8c6dd846db7c684408ea62
nwdelafu <nick.delafuente1@gmail.com>
2021-06-15 17:30:35 -0700

    Program basic functionality complete - moving to customizability and cutting O

--- challenge.py
+++ challenge.py
@@ -9,32 +9,31 @@ def main():
     Main function call for stdin and stdout.
     """
     inputs = []
-    running_mean = float()
-    running_sd = float()
-    running_median = float()
+    # # Save on that 'big O':
+    # running_mean = float()
+    # running_sd = float()
+    # running_median = float()
 
     while True:
-        user_input = input("Enter a number: ")
+        user_input = input("Enter a number ('q' to quit): ")
         if user_input == "q":
             break
-        if isinstance(user_input, (float, int)):
-            input.append(user_input)
+        try:
+            inputs.append(float(user_input))
             print(f"""
-                Mean: {}
-                Standard Deviation: {}
-                Median: {}
-
-                Press 'q' to quit.
+            Mean: {mean(inputs)}
+            Standard Deviation: {stdev(inputs)}
+            Median: {median(inputs)}
             """)
-        else:
-            print("""
-            Please enter a valid int or float value.
-
-            Press 'q' to quit.
-            """)
+        except (ValueError, TypeError):
+            return "Enter an int or float."
 
 
 # Could use statistics but we'll just code it ¯\_( ͡❛ ͜ʖ ͡❛)_/¯
+def mean(data):
+    return sum(data) / len(data)
+
+
 def variance(data, ddof=0):
     """
     - data is array-like
@@ -53,6 +52,20 @@ def stdev(data):
     var = variance(data)
     std_dev = math.sqrt(var)
     return std_dev
+
+
+def median(data):
+    """
+    - data is array-like
+    """
+    sorted_data = sorted(data)
+    data_len = len(data)
+    index = (data_len - 1) // 2
+
+    if (data_len % 2):
+        return sorted_data[index]
+    else:
+        return (sorted_data[index] + sorted_data[index + 1]) / 2.0
 
 
 if __name__ == "__main__":
